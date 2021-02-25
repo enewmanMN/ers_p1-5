388361926cd5c31ada37902499534af1d07357f9
Eric Newman <googlnewman@gmail.com>
2021-02-25 09:24:55 -0600

    Trying elis cfg info

--- src/main/java/com/revature/util/HibernateUtil.java
+++ src/main/java/com/revature/util/HibernateUtil.java
@@ -1,20 +1,29 @@
 package com.revature.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import java.io.File;
 
 public class HibernateUtil
 {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory()
     {
         try
         {
+            if (sessionFactory == null) {
+                StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+                Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+                sessionFactory = metaData.getSessionFactoryBuilder().build();
+            }
             // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure(new File("hibernate.cfg.xml")).buildSessionFactory();
+            return sessionFactory;
         }
 
         catch (Throwable ex) {
